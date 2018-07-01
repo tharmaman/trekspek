26daf7f273e6733aca99bfa98fcbef4a5f11a199
Danny Tharma <dltcorreo@gmail.com>
2018-07-01 17:54:35 +0000

    added hardcoded geocoder API and installed geocoder module

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -139,7 +139,7 @@ var NodeGeocoder = require('node-geocoder');
 var options = {
   provider: 'google',
   httpAdapter: 'https',
-  apiKey: process.env.GEOCODER_API_KEY,
+  apiKey: "AIzaSyD4E8Rdmen6TtUFIvUqx4YEyJ3mdxnNXLk",
   formatter: null
 };
  
