d342f54132bc02624d11f4109c1377e8e6a3bcef
tharmaman <dltcorreo@gmail.com>
2018-07-08 22:55:33 +0000

    added forgot password reset feature

--- models/user.js
+++ models/user.js
@@ -2,14 +2,20 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String, unique: true, required: true
+    },
     password: String,
     avatar: {
         type: String, default: "/media/default_profile.png"   
     },
     firstName: String,
     lastName: String,
-    email: String,
+    email: {
+        type: String, unique: true, required: true
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     bio: String,
     isAdmin: {
         type: Boolean, default: false
